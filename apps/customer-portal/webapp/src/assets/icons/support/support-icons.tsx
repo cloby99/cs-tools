2d2989fd7e4ccfc4eb0eaeadd73c1d51dd357b2e
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2025-12-18 14:36:45 +0530

    Add license headers to support icon components
    Added Apache 2.0 license headers to all support icon component files in the customer portal webapp. Also commented out all exports in support-icons.tsx, likely to temporarily disable re-exporting these icons.

--- apps/customer-portal/webapp/src/assets/icons/support/support-icons.tsx
+++ apps/customer-portal/webapp/src/assets/icons/support/support-icons.tsx
@@ -14,50 +14,34 @@
 // specific language governing permissions and limitations
 // under the License.
 
-// Copyright (c) 2025 WSO2 LLC. (https://www.wso2.com).
-//
-// WSO2 LLC. licenses this file to you under the Apache License,
-// Version 2.0 (the "License"); you may not use this file except
-// in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
-
-export * from "./ArrowLeftIcon";
-export * from "./ArrowRightIcon";
-export * from "./BookOpenIcon";
-export * from "./BotIcon";
-export * from "./CalendarDaysIcon";
-export * from "./CalendarIcon";
-export * from "./ChevronDownIcon";
-export * from "./ChevronUpIcon";
-export * from "./CircleAlertIcon";
-export * from "./CircleCheckIcon";
-export * from "./CirclePauseIcon";
-export * from "./CirclePlayIcon";
-export * from "./CircleXIcon";
-export * from "./ClockIcon";
-export * from "./ExternalLinkIcon";
-export * from "./FileTextIcon";
-export * from "./InfoIcon";
-export * from "./Maximize2Icon";
-export * from "./MessageCircleIcon";
-export * from "./MessageSquareIcon";
-export * from "./PaperclipIcon";
-export * from "./PhoneIcon";
-export * from "./PhoneCallIcon";
-export * from "./PlayIcon";
-export * from "./SearchIcon";
-export * from "./SendIcon";
-export * from "./ServerIcon";
-export * from "./SlidersHorizontalIcon";
-export * from "./TrendingUpIcon";
-export * from "./UserIcon";
-export * from "./XIcon";
+// export * from "./ArrowLeftIcon";
+// export * from "./ArrowRightIcon";
+// export * from "./BookOpenIcon";
+// export * from "./BotIcon";
+// export * from "./CalendarDaysIcon";
+// export * from "./CalendarIcon";
+// export * from "./ChevronDownIcon";
+// export * from "./ChevronUpIcon";
+// export * from "./CircleAlertIcon";
+// export * from "./CircleCheckIcon";
+// export * from "./CirclePauseIcon";
+// export * from "./CirclePlayIcon";
+// export * from "./CircleXIcon";
+// export * from "./ClockIcon";
+// export * from "./ExternalLinkIcon";
+// export * from "./FileTextIcon";
+// export * from "./InfoIcon";
+// export * from "./Maximize2Icon";
+// export * from "./MessageCircleIcon";
+// export * from "./MessageSquareIcon";
+// export * from "./PaperclipIcon";
+// export * from "./PhoneIcon";
+// export * from "./PhoneCallIcon";
+// export * from "./PlayIcon";
+// export * from "./SearchIcon";
+// export * from "./SendIcon";
+// export * from "./ServerIcon";
+// export * from "./SlidersHorizontalIcon";
+// export * from "./TrendingUpIcon";
+// export * from "./UserIcon";
+// export * from "./XIcon";
